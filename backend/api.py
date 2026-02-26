e696220348218de5af8d85ba787576b384d13b21
Bella <bellalannermuller@gmail.com>
2026-02-26 12:32:54 +0100

    Naming convention for endpoints

---
+++ backend/api.py
@@ -0,0 +1,11 @@
+# GET /users - hämta alla
+# GET /users/{id} - hämta en
+# POST /users - skapa
+# PATCH /users/{id} - uppdatera
+# DELETE /users/{id} - ta bort
+
+# get_user()
+# get_all_users()
+# create_user()
+# update_user()
+# delete_user()
