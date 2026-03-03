e5b43515db321621e89c8380540ceede12e0e7f1
Bella <bellalannermuller@gmail.com>
2026-03-03 13:28:30 +0100

    Add queries for characters

--- backend/db.py
+++ backend/db.py
@@ -22,6 +22,7 @@ def create_world(connection, world_name, world_description):
             new_world = cursor.fetchone()
         return new_world 
 
+
 def get_all_worlds(connection):
     with connection:
         with connection.cursor(cursor_factory=RealDictCursor) as cursor:
@@ -33,6 +34,7 @@ def get_all_worlds(connection):
             )
             all_worlds = cursor.fetchall()
         return all_worlds
+
 
 def get_world_by_id(connection, world_id):
     with connection:
@@ -48,6 +50,7 @@ def get_world_by_id(connection, world_id):
             world = cursor.fetchone()
         return world
 
+
 def update_world(connection, world_id, world_name=None, world_description=None):
     with connection:
         with connection.cursor(cursor_factory=RealDictCursor) as cursor:
@@ -59,10 +62,11 @@ def update_world(connection, world_id, world_name=None, world_description=None):
                 WHERE world_id = %s
                 RETURNING *;
                 """,
-                (world_id, world_name, world_description)
+                (world_name, world_description, world_id)
             )
             updated_world = cursor.fetchone()
         return updated_world
+
 
 def delete_world(connection, world_id):
     with connection:
@@ -79,9 +83,89 @@ def delete_world(connection, world_id):
             deleted_world = cursor.fetchone()
         return deleted_world
 
-# Maps
-
 # Characters
+def create_character(connection, world_id, character_name, character_description, is_alive=True, image_id=None, species_id=None, item_id=None):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """
+                INSERT INTO characters (world_id, character_name, character_description, is_alive, image_id, species_id, item_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s) 
+                RETURNING *; 
+                """,
+                (world_id, character_name, character_description, is_alive, image_id, species_id, item_id)
+            )
+            new_character = cursor.fetchone()
+        return new_character 
+
+
+def get_all_characters(connection):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """
+                SELECT *
+                FROM characters;
+                """
+            )
+            all_characters = cursor.fetchall()
+        return all_characters
+
+
+def get_character_by_id(connection, character_id):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """
+                SELECT *
+                FROM characters
+                WHERE character_id = %s;
+                """,
+                (character_id,)
+            )
+            character = cursor.fetchone()
+        return character
+
+
+def update_character(connection, character_id, character_name=None, character_description=None, is_alive=None, image_id=None, species_id=None, item_id=None):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """
+                UPDATE characters
+                SET 
+                    character_name = COALESCE (%s, character_name),
+                    character_description = COALESCE (%s, character_description),
+                    is_alive = COALESCE (%s, is_alive),
+                    image_id = COALESCE (%s, image_id),
+                    species_id = COALESCE (%s, species_id),
+                    item_id = COALESCE (%s, item_id)
+                WHERE character_id = %s
+                RETURNING *;
+                """,
+                (character_name, character_description, is_alive, image_id, species_id, item_id, character_id)
+            )
+            updated_character = cursor.fetchone()
+        return updated_character
+
+
+def delete_character(connection, character_id):
+    with connection:
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                """
+                DELETE
+                FROM characters
+                WHERE character_id = %s
+                RETURNING *;
+                """,
+                (character_id)
+            )
+            deleted_character = cursor.fetchone()
+        return deleted_character
+
+
+# Maps
 
 # Relationships 
 
